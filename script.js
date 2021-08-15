36759a9058dfd4b1241ae3810eb54430716614be
Sarah Edison <edison.sarahc@gmail.com>
2021-08-15 10:13:42 -0400

    Work on eventlistener

--- script.js
+++ script.js
@@ -12,22 +12,29 @@ const recipes = {
 }
 
 //Create a dropdown list from the recipes array. Use for in statement to add objects as dropdown options. 
-function chooseRecipe(recipes){
-    var select = document.getElementById('arr');
+function createRecipes(recipes){
+    const select = document.getElementById('select');
     for(let recipe in recipes){
-        var dropdown = document.createElement("option");
+        let dropdown = document.createElement("option");
         dropdown.textContent = recipe;
         dropdown.innerHTML = recipe;
         select.appendChild(dropdown);
     }
+    
 }
+
+//Choose an option from the dropdown and submit it through button click. 
+document.getElementById('submit').addEventListener("click", displayRecipe);
+
+//Option choice becomes the argument for the displayRecipe functions and executes function. 
+
 
 //Run through the recipe array and create a list of ingredients with a serving size
 function displayRecipe(recipe){
 
     for(let i=0; i<recipe.length; i++){
 
-        var item = document.createElement("li");
+        let item = document.createElement("li");
         item.innerHTML = recipe[i].ingredient + " " + recipe[i].serving + " " + recipe[i].measurement;
         document.body.appendChild(item);
          
@@ -37,6 +44,4 @@ function displayRecipe(recipe){
     };
   }
 
-chooseRecipe(recipes);
-displayRecipe(recipes.margarita);
-displayRecipe(recipes.oldFashioned);
+createRecipes(recipes)
