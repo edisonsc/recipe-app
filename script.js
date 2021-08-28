10eaa62a3f1dfc9fbd32024ff7a419f37086b4d8
Sarah Edison <edison.sarahc@gmail.com>
2021-08-28 11:16:02 -0400

    feat: add choose option function

--- script.js
+++ script.js
@@ -8,30 +8,41 @@ const recipes = {
     { ingredient: "sugar", serving: 1, measurement: "tsp" },
     { ingredient: "bourbon", serving: 2, measurement: "oz" },
     { ingredient: "bitters", serving: 1, measurement: "tsp" },
+],
+
+    newDrink: [
+    { ingredient: "sugar", serving: 1, measurement: "tsp" },
+    { ingredient: "bourbon", serving: 2, measurement: "oz" },
+    { ingredient: "bitters", serving: 1, measurement: "tsp" },
 ]
 }
+const select = document.getElementById('select');
 
 //Create a dropdown list from the recipes array. Use for in statement to add objects as dropdown options. 
 function createRecipes(recipes){
-    const select = document.getElementById('select');
     for(let recipe in recipes){
         let dropdown = document.createElement("option");
-        dropdown.textContent = recipe;
         dropdown.innerHTML = recipe;
         select.appendChild(dropdown);
     }
-    
 }
 
 //Choose an option from the dropdown and submit it through button click. 
-document.getElementById('submit').addEventListener("click", displayRecipe);
+function chooseOption (recipes){
+    let x = select.selectedIndex;
+    let choice = select.options;
+    let list = choice[x].value;
+    console.log(list);
+   }
+
+document.getElementById('submit').addEventListener("click", chooseOption);
 
 //Option choice becomes the argument for the displayRecipe functions and executes function. 
 
 
-//Run through the recipe array and create a list of ingredients with a serving size
-function displayRecipe(recipe){
 
+//Run through the recipe array and create a list of ingredients with a serving size
+function displayRecipe(recipe){ 
     for(let i=0; i<recipe.length; i++){
 
         let item = document.createElement("li");
@@ -44,4 +55,5 @@ function displayRecipe(recipe){
     };
   }
 
-createRecipes(recipes)
+createRecipes(recipes);
+
